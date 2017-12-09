b5f9fc5ff68f552bb2e6d4f7f733dff28bec842c
Teresa Margarida Baptista Cabral De Carvalho Salazar <tmsalazar@student.dei.uc.pt>
2017-12-09 19:43:14 +0000

    Styles vote's pages.

--- ivotas/app.py
+++ ivotas/app.py
@@ -38,6 +38,7 @@ def admin():
         return render_template('admin.html')
     return redirect(url_for('login_admin'))
 
+
 @app.route('/login_admin', methods=['GET', 'POST'])
 def login_admin():
     form = forms.AuthenticateUserForm(request.form)
