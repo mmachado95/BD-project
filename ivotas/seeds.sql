2e3ed9e9f2088c7a438f91fa0efa301ec3cf2cb0
Teresa Margarida Baptista Cabral De Carvalho Salazar <tmsalazar@student.dei.uc.pt>
2017-12-10 22:08:04 +0000

    Final

--- ivotas/seeds.sql
+++ ivotas/seeds.sql
@@ -44,32 +44,32 @@ VALUES
 
 INSERT INTO Eleicao(nome, descricao, inicio, fim, tipo)
 VALUES
-  ('CG 2', 'Conselho Geral 2', '2017-11-20 22:00:00', '2017-12-22 10:15:00', 1), --> id: 1
-  ('CG 3', 'Conselho Geral 3', '2017-12-20 22:00:00', '2017-12-21 10:15:00', 1); --> id: 2
+  ('CG 2', 'Conselho Geral 2', '2017-11-20 22:00:00', '2017-12-22 10:15:00', 1),
+  ('CG 3', 'Conselho Geral 3', '2017-12-20 22:00:00', '2017-12-21 10:15:00', 1);
 
 INSERT INTO Eleicao(nome, descricao, inicio, fim, tipo, total_votos, votos_brancos, votos_nulos)
 VALUES
-  ('CG 1', 'Conselho Geral', '2017-11-20 22:00:00', '2017-11-13 10:15:00', 1, 6, 1, 1); --> id: 3 PASSADA
+  ('CG 1', 'Conselho Geral', '2017-11-20 22:00:00', '2017-11-13 10:15:00', 1, 6, 1, 1);
 
 INSERT INTO Eleicao(nome, unidade_organica_id, descricao, inicio, fim, tipo)
 VALUES
-  ('NEI', 3, 'Nucleo de Estudantes de Informatica', '2017-11-20 22:00:00', '2017-12-22 10:15:00', 2), --> id: 4
-  ('NEG', 6, 'Nucleo de Estudantes de Gestao', '2017-12-20 22:00:00', '2017-12-21 10:15:00', 2); --> id: 5
+  ('NEI', 3, 'Nucleo de Estudantes de Informatica', '2017-11-20 22:00:00', '2017-12-22 10:15:00', 2),
+  ('NEG', 6, 'Nucleo de Estudantes de Gestao', '2017-12-20 22:00:00', '2017-12-21 10:15:00', 2);
 
 INSERT INTO Eleicao(nome, unidade_organica_id, descricao, inicio, fim, tipo, total_votos, votos_brancos, votos_nulos)
 VALUES
-  ('NEM', 4, 'Nucleo de Estudantes de Mecanica', '2017-11-20 22:00:00', '2017-11-13 10:15:00', 2, 4, 2, 1); --> id: 6 PASSADA
+  ('NEM', 4, 'Nucleo de Estudantes de Mecanica', '2017-11-20 22:00:00', '2017-11-13 10:15:00', 2, 4, 2, 1);
 
 INSERT INTO Lista(eleicao_id, nome, numero_votos)
 VALUES
-  (6, 'LISTA NEM A', 1), --> 1 voto
-  (6, 'LISTA NEM B', 0), --> 1 voto
-  (3, 'LISTA CG1 ALUNOS C', 0), --> 0 votos
-  (3, 'LISTA CG1 ALUNOS D', 2), --> 2 votos
-  (3, 'LISTA CG1 DOCENTES A', 0), --> 0 votos
-  (3, 'LISTA CG1 DOCENTES B', 1), --> 1 voto
-  (3, 'LISTA CG1 FUNCIONARIOS X', 0), --> 0 voto
-  (3, 'LISTA CG1 FUNCIONARIOS Z', 1); --> 1 voto
+  (6, 'LISTA NEM A', 1),
+  (6, 'LISTA NEM B', 0),
+  (3, 'LISTA CG1 ALUNOS C', 0),
+  (3, 'LISTA CG1 ALUNOS D', 2),
+  (3, 'LISTA CG1 DOCENTES A', 0),
+  (3, 'LISTA CG1 DOCENTES B', 1),
+  (3, 'LISTA CG1 FUNCIONARIOS X', 0),
+  (3, 'LISTA CG1 FUNCIONARIOS Z', 1);
 
 INSERT INTO Lista(eleicao_id, nome)
 VALUES
@@ -95,6 +95,7 @@ VALUES
 
 INSERT INTO MesaDeVoto(eleicao_id, unidade_organica_id)
 VALUES
+  (4, 1),
   (6, 1),
   (3, 1);
 
@@ -104,16 +105,15 @@ VALUES
   (2),
   (2);
 
--- 1 ao 8 9 ao 12 13 ao 16
 INSERT INTO Voto(pessoa_id, mesa_de_voto_id, momento)
 VALUES
-  (3, 1, '2017-11-20 22:00:03'), -- aluno a votar em branco
-  (4, 1, '2017-11-20 22:00:04'), -- aluno a votar em nulo
-  (3, 1, '2017-11-20 22:00:03'), -- aluno a votar em branco
-  (4, 1, '2017-11-20 22:00:04'), -- aluno a votar em nulo
-  (10, 2, '2017-11-20 22:00:04'), -- docente a votar em lista
-  (14, 2, '2017-11-20 22:00:04'), -- funcionario a votar em lista
-  (5, 2, '2017-11-20 22:00:04'), -- aluno a votar em lista
-  (6, 2, '2017-11-20 22:00:04'), -- aluno a votar em lista
-  (12, 2, '2017-11-20 22:00:04'), -- random branco
-  (13, 2, '2017-11-20 22:00:04');
+  (3, 2, '2017-11-20 22:00:03'),
+  (4, 2, '2017-11-20 22:00:04'),
+  (3, 2, '2017-11-20 22:00:03'),
+  (4, 2, '2017-11-20 22:00:04'),
+  (10, 3, '2017-11-20 22:00:04'),
+  (14, 3, '2017-11-20 22:00:04'),
+  (5, 3, '2017-11-20 22:00:04'),
+  (6, 3, '2017-11-20 22:00:04'),
+  (12, 3, '2017-11-20 22:00:04'),
+  (13, 3, '2017-11-20 22:00:04');
